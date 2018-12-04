d3cf22e3f338759e2a36776c204e57b3304878e1
Adley <adley.salabi@gmail.com>
2018-12-04 12:08:29 +0100

    modified the main.go for the port

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-
+	"os"
 	"wsf/devops/handler"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// connection := os.Getenv("DATABASE_URL")
+	fmt.Println("Tout va bien")
+	fmt.Println("Everything is going well well")
+
+	fmt.Println("Server is running at ", port)
 
 	myHandler := handler.NewHandler()
 
